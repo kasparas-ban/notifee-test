a8ed582ff4331b5dbe4145b758e2555eaed5daaa
kasparas-ban <kasparas.baniulis@gmail.com>
2024-12-22 13:19:06 +0200

    Working app #4

--- app/(tabs)/goals.tsx
+++ app/(tabs)/goals.tsx
@@ -1,8 +1,12 @@
 import { StyleSheet, View } from "react-native";
-// import ItemListWrapper from "@/components/ItemList/ItemListWrapper"
+import ItemListWrapper from "@/components/ItemList/ItemListWrapper";
 
 export default function GoalsScreen() {
-  return <View style={styles.container}>{/* <ItemListWrapper /> */}</View>;
+  return (
+    <View style={styles.container}>
+      <ItemListWrapper />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
